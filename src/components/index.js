52b5b69eaff9768fe45a408c476eddbbbc4a4e42
Robert Haynes <robhaynes0420@gmail.com>
2024-02-29 19:57:45 +0000

    Changed path

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Hero from '../components/Hero';
+import Overview from '../components/Overview';
+import ParallaxComponent from '../components/Parallax';
+
+export { Hero, Overview, ParallaxComponent };
--- src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-import Hero from './Hero';
-import Overview from './Overview';
-import ParallaxComponent from './Parallax';
-
-export { Hero, Overview, ParallaxComponent };
